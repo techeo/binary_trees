82a31eb796310c3936ee5e53dc713996ed776bfe
techeo <tegaecheo@gmail.com>
2024-03-27 11:52:49 -0700

    correct 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder -  goes through a binary tree using post-order traversal
+ * binary_tree_postorder -  goes through tree using post-order traversal
  * @tree: tree to go through
  * @func: function to use
  * Return: Nothing
